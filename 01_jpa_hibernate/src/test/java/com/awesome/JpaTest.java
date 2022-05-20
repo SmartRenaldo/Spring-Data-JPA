0a34b7535cc05617d6589fa65047d4453682f942
SmartRenaldo <correctgavin@gmail.com>
2022-05-20 19:37:29 +0930

    jpa

--- 01_jpa_hibernate/src/test/java/com/awesome/JpaTest.java
+++ 01_jpa_hibernate/src/test/java/com/awesome/JpaTest.java
@@ -111,4 +111,63 @@ public class JpaTest {
 
         tx.commit();
     }
+
+    @Test
+    public void testStatus01() {
+        EntityManager em = factory.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Customer customer = new Customer(); //Transient state
+        customer.setId(5L); //Detached state
+
+        customer = em.find(Customer.class, 5L); //Persistent (JPA managed) state
+
+        em.remove(customer); //Removed object state
+
+        tx.commit();
+    }
+
+    @Test
+    public void testStatus02() {
+        EntityManager em = factory.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Customer customer = new Customer(); //Transient state
+
+        customer = em.find(Customer.class, 7L); //Persistent (JPA managed) state
+        customer.setAddress("Paris");
+
+        tx.commit();
+    }
+
+    @Test
+    public void testStatus03() {
+        EntityManager em = factory.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        Customer customer = em.find(Customer.class, 8L); //Persistent (JPA managed) state
+
+        em.remove(customer);
+        em.persist(customer);
+
+        tx.commit();
+    }
+
+    @Test
+    public void testCache() {
+        EntityManager em = factory.createEntityManager();
+        Customer customer = em.find(Customer.class, 8L); //Persistent (JPA managed) state
+        Customer customer2 = em.find(Customer.class, 8L); //Persistent (JPA managed) state
+    }
+
+    @Test
+    public void testCache2() {
+        EntityManager em = factory.createEntityManager();
+        Customer customer = em.find(Customer.class, 8L); //Persistent (JPA managed) state
+
+        em = factory.createEntityManager();
+        Customer customer2 = em.find(Customer.class, 8L); //Persistent (JPA managed) state
+    }
 }
