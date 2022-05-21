9cb19d0d192b0ced5cf5578d2e809e08b66cacc4
SmartRenaldo <correctgavin@gmail.com>
2022-05-21 16:48:05 +0930

    querydsl

--- 03_springdata_jpa_querydsl/src/test/java/com/awesome/test/QueryDSLTest.java
+++ 03_springdata_jpa_querydsl/src/test/java/com/awesome/test/QueryDSLTest.java
@@ -1,14 +1,26 @@
 package com.awesome.test;
 
 import com.awesome.config.ApplicationConfig;
+import com.awesome.pojo.Customer;
 import com.awesome.pojo.QCustomer;
 import com.awesome.repositories.CustomerRepositoryQueryDSL;
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @ContextConfiguration(classes = ApplicationConfig.class)
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -18,7 +30,7 @@ public class QueryDSLTest {
     CustomerRepositoryQueryDSL repository;
 
     @Test
-    public void testEQ(){
+    public void testEQ() {
         QCustomer customer = QCustomer.customer;
 
         BooleanExpression eq = customer.id.eq(10L);
@@ -30,7 +42,7 @@ public class QueryDSLTest {
      * find customer id > 5, name = "Isa" | "Nill"
      */
     @Test
-    public void testAnd(){
+    public void testAnd() {
         QCustomer customer = QCustomer.customer;
 
         BooleanExpression and = customer.id.gt(5L)
@@ -38,6 +50,54 @@ public class QueryDSLTest {
                 .and(customer.address.eq("London"));
 
         System.out.println(repository.findAll(and));
+    }
+
+    @Test
+    public void testDynamicSearch() {
+        Customer params = new Customer();
+        params.setName("Greenish, Isa, Opera, Tira");
+        params.setAddress("Sydney");
+        params.setId(5L);
+
+        // initialised condition, always works
+        QCustomer customer = QCustomer.customer;
+
+        BooleanExpression expression = customer.isNotNull().or(customer.isNull());
+
+        expression = (params.getId() != null && params.getId() > -1) ?
+                expression.and(customer.id.gt(params.getId())):expression;
+        expression = StringUtils.hasText(params.getName()) ?
+                expression.and(customer.name.in(params.getName().split(", "))):expression;
+        expression = StringUtils.hasText(params.getAddress()) ?
+                expression.and(customer.address.eq(params.getAddress())):expression;
+
+        System.out.println(repository.findAll(expression));
+    }
+
+    //@PersistenceContext could replace @Autowired to solve thread safety issue
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Test
+    public void testNativeSQL() {
+        Customer params = new Customer();
+        params.setName("Greenish, Isa, Opera, Tira");
+        params.setAddress("Sydney");
+        params.setId(5L);
+
+        JPAQueryFactory factory = new JPAQueryFactory(entityManager);
+
+        QCustomer customer = QCustomer.customer;
+
+        JPAQuery<Tuple> tuple = factory.select(customer.count(), customer.id, customer.name)
+                .from(customer)
+                .where(customer.id.gt(params.getId()))
+                .groupBy(customer.address)
+                .orderBy(customer.id.desc());
+
+        for (Tuple fetch : tuple.fetch()) {
+            System.out.println(fetch.get(customer.count()) + " " + fetch.get(customer.id) + " " + fetch.get(customer.name));
+        }
     }
 
 }
